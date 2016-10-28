10d9f7ff11a4dcde71ca1a001040e52cb10abbdd
Greg Woods <greg.woods@moserit.com>
2016-10-28 12:02:30 -0400

    Improve spud user spec

--- spec/models/has_api_keys_spec.rb
+++ spec/models/has_api_keys_spec.rb
@@ -2,14 +2,33 @@ require 'rails_helper'
 
 RSpec.describe SpudUser, type: :model do
 
+  before :each do
+    @user = FactoryGirl.create(:spud_user)
+    @count = 3
+    @count.times { FactoryGirl.create(:tb_api_key, spud_user: @user) }
+  end
+
+  describe 'has_many' do
+    it 'should have many keys' do
+      expect(@user.api_keys.count).to eq(@count)
+    end
+
+    context 'when the user is deleted' do
+      it 'should delete associated keys' do
+        expect do
+          @user.destroy
+        end.to change(TbApiKey, :count).by(-@count)
+      end
+    end
+  end
+
   describe '#invalidate_api_keys' do
-    it 'should destroy all related keys' do
-      user = FactoryGirl.create(:spud_user)
-      count = 3
-      count.times { FactoryGirl.create(:tb_api_key, spud_user: user) }
-      expect do
-        user.update(password: 'new-password', password_confirmation: 'new-password')
-      end.to change(TbApiKey, :count).by(-count)
+    context 'when the user changes their password' do
+      it 'should delete associated keys' do
+        expect do
+          @user.update(password: 'new-password', password_confirmation: 'new-password')
+        end.to change(TbApiKey, :count).by(-@count)
+      end
     end
   end
 end
